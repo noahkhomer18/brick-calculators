69031a89cfbe0fe556ab5efdfcbaa136d182cf5a
noahkhomer18 <noahkhomer18@users.noreply.github.com>
2025-11-12 17:15:00 -0600

    Add App.jsx component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import BrickCalculator from './components/BrickCalculator'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-background">
+      <BrickCalculator />
+    </div>
+  )
+}
+
+export default App
+
